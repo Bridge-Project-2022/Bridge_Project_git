63ebf372d46b99b7cdc999edf8e4c6c03bb9b091
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2022-11-09 04:35:13 +0900

    Json Manager Modify

---
+++ Assets/JA/Scripts/Class/Days.cs
@@ -0,0 +1,7 @@
+using System;
+
+[Serializable]
+public class Days
+{
+    public Day[] day;
+}
--- Assets/JA/Scripts/Test.cs
+++ Assets/JA/Scripts/Test.cs
@@ -6,12 +6,19 @@ public class Test : MonoBehaviour
 {
     void Start()
     {
-        //Debug.Log(CustomerManager.Instance.days[0].customer[0].dialogue.ResultGoodComment[1]);
+       Customer a = CustomerManager.Instance.days.day[0].customer[0];
+       
+       print(a.id);
+       print(a.name);
+       print(a.criminalGuest);
+       print(a.uniqueGuest);
+       foreach (var VARIABLE in a.dialogue.refusalComment)
+       {
+           print(VARIABLE);
+       }
 
-        foreach (var v in CustomerManager.Instance.days[0].customer[0].dialogue.ResultGoodComment)
-        {
-            Debug.Log(v);
-        }
+        //foreach (var v in CustomerManager.Instance.days[0].customer[0].dialogue.ResultGoodComment)
+        //
     }
 
     // Update is called once per frame
